3808a4bd57f7159d59cc224eee0e1ee2530bdf51
Allan Gao <allangao95@gmail.com>
2018-08-27 17:04:24 +1000

    add balance withdraw deposit feature

--- banking_challenge.rb
+++ banking_challenge.rb
@@ -1,26 +1,67 @@
 password = "password"
 balance = 0
 
-while password == "password" do
 puts "Enter your password"
-    askpass = gets.chomp
 
-    if askpass == "password"
+while askpass = gets.chomp
+    if askpass != "password"
+    puts "Invalid password, try again"
+    elsif
+        askpass == "password"
+
     puts "Welcome to the banking app"
-    puts "Your balance is #{balance}"
-    puts "What would you like to do?"
-    puts "options: balance  deposit"
+    puts "Your balance is $#{balance}"
+    
 
-        while balance == 0 do
+        while balance >= 0 do
+            puts "What would you like to do?"
+            puts "options: balance  deposit  withdraw"
+
         response = gets.chomp
-
             if response == "balance"
-            puts "Your balance is #{balance}"
-            else
-            puts "Invalid selection!"
+            puts "Your balance is $#{balance}"
+            elsif response == "deposit" 
+                puts "Enter the amount to deposit"
+                deposit_amount = gets.chomp
+                balance += deposit_amount.to_i
+                puts "Your balance is now $#{balance}"
+            elsif response == "withdraw" 
+                puts "Enter the amount to withdraw"
+                withdraw_amount = gets.chomp
+                if balance >= withdraw_amount.to_i
+                    balance -= withdraw_amount.to_i
+                    puts "Your balance is now $#{balance}"
+                else
+                    puts "You have insufficient funds"
+                end
             end
         end
     end
-    puts "Invalid password"
 end
+
+
+# password = "password"
+# balance = 0
+
+# while password == "password" do
+# puts "Enter your password"
+#     askpass = gets.chomp
+
+#     if askpass == "password"
+#     puts "Welcome to the banking app"
+#     puts "Your balance is #{balance}"
+#     puts "What would you like to do?"
+#     puts "options: balance  deposit"
+
+#         while balance == 0 do
+#         response = gets.chomp
+#             if response == "balance" or "Balance"
+#                 puts "Your balance is #{balance}"
+#             else
+#             puts "Invalid selection!"
+#             end
+#         end
+#     end
+#     puts "Invalid password"
+# end
 
