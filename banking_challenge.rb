d7498ddc34b8672fe452f4588af4b6eede2e0de0
Allan Gao <allangao95@gmail.com>
2018-08-27 17:05:35 +1000

    add feature balance withdraw deposit

--- banking_challenge.rb
+++ banking_challenge.rb
@@ -40,28 +40,4 @@ while askpass = gets.chomp
 end
 
 
-# password = "password"
-# balance = 0
-
-# while password == "password" do
-# puts "Enter your password"
-#     askpass = gets.chomp
-
-#     if askpass == "password"
-#     puts "Welcome to the banking app"
-#     puts "Your balance is #{balance}"
-#     puts "What would you like to do?"
-#     puts "options: balance  deposit"
-
-#         while balance == 0 do
-#         response = gets.chomp
-#             if response == "balance" or "Balance"
-#                 puts "Your balance is #{balance}"
-#             else
-#             puts "Invalid selection!"
-#             end
-#         end
-#     end
-#     puts "Invalid password"
-# end
 
